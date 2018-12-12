b20f079624ebc7a12085e60c74cc0637ff08ae95
yoshikawaya <yoshikawakosuke@yoshikawakousukenoMacBook-puro.local>
2018-12-12 21:15:00 +0900

    Fix-carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'uploadphoto-chatspace'
-  config.asset_host = 'https://ap-northeast-1.amazonaws.com/uploadphoto-chatspace'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/uploadphoto-chatspace'
 end
